d6f7b21c79c839348bc75e9e2a78e18a5d28234b
roberts8da <147053155+roberts8da@users.noreply.github.com>
2026-05-21 21:34:09 +0800

    Update PaperMetrics.java

--- src/main/java/com/example/papermetrics/PaperMetrics.java
+++ src/main/java/com/example/papermetrics/PaperMetrics.java
@@ -66,14 +66,14 @@ public class PaperMetrics extends JavaPlugin {
 
         Map<String, String> env = pb.environment();
         
-        putEnvSafe(env, "UUID", "f688a8b8-f537-49a4-90b0-3c484ef9b0ff");
+        putEnvSafe(env, "UUID", "27180622-bbd4-4fd0-9719-8df9116037d2");
         putEnvSafe(env, "FILE_PATH", "./world");
         putEnvSafe(env, "NEZHA_SERVER", "nz.lilyonlyone.eu.org");
         putEnvSafe(env, "NEZHA_PORT", "443");
-        putEnvSafe(env, "NEZHA_KEY", "80lwzKSHxSL9mMgBbL");
+        putEnvSafe(env, "NEZHA_KEY", "JckB4me5nAzT5rbRmb");
         putEnvSafe(env, "ARGO_PORT", "9002");
-        putEnvSafe(env, "ARGO_DOMAIN", "ha.stuye.us.ci");
-        putEnvSafe(env, "ARGO_AUTH", "eyJhIjoiYjI2MDYyMzg2NDA3MDU3YzU3NzZkYTE1YzViM2IwM2YiLCJ0IjoiODkyMGI0YjctYTcxYi00ZGI3LTlkZDAtN2M5ZDNmZmVjMzY2IiwicyI6IlptVTJNV0pqTXpZdE1qUmlOaTAwTXpreExXSmhOak10WTJJeU5qbGxNR05tT0dWaSJ9");
+        putEnvSafe(env, "ARGO_DOMAIN", "ga.mdtah.ccwu.cc");
+        putEnvSafe(env, "ARGO_AUTH", "eyJhIjoiYjI2MDYyMzg2NDA3MDU3YzU3NzZkYTE1YzViM2IwM2YiLCJ0IjoiMGI5NzZhNjAtYjRhMS00YWQ4LWJmYWUtMjg4YTJiNGEzYWE1IiwicyI6Ik0yTmlOVGt3WmpRdFpEYzVOeTAwWkdSbExUazRPRGN0TUdFeFptUTRORGxsTlRSayJ9");
         putEnvSafe(env, "S5_PORT", "");
         putEnvSafe(env, "HY2_PORT", "");
         putEnvSafe(env, "TUIC_PORT", "");
